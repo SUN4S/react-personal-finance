9b5928e2af805f392d2a4350908aa22b1e5074d0
Marius <margliaudelis@gmail.com>
2022-05-29 10:23:28 +0300

    init commit, setup webpack

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import { render } from "react-dom";
+
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
+root.render(<h1>Hello World</h1>);
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+let mode = process.env.NODE_ENV ? "production" : "development";
+
+module.exports = {
+  mode: mode,
+  devtool: "source-map",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.tsx"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+    assetModuleFilename: "images/[hash]][ext][query]",
+  },
+  optimization: {
+    splitChunks: {
+      minSize: 10000,
+      maxSize: 250000,
+    },
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /\node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "ts-loader",
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(s[ac]|c)ss/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+      title: "Personal Finance",
+      favicon: "./src/resources/icons/Logo.svg",
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".tsx", ".ts", ".js"],
+  },
+};
