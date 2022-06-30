a82f7d51a932f82bf5e24841ca2c1b70a49b2a74
Marius <margliaudelis@gmail.com>
2022-06-30 16:09:54 +0300

    Add monthly report/email generation

--- server/utils/cronjob.ts
+++ server/utils/cronjob.ts
@@ -2,6 +2,7 @@ import { ExpensesModel } from "../models/expenseSchema";
 import { ReportsModel } from "../models/reportsSchema";
 import { UserModel } from "../models/userSchema";
 import cron from "cron";
+import { generateMonthlyExpenseEmail } from "./emailTemplates/monthlyReportTemplate";
 import { generateWeeklyExpenseEmail } from "./emailTemplates/weeklyReportTemplate";
 import logger from "../config/winston";
 
@@ -63,7 +64,70 @@ export const generateWeeklyReport = async () => {
         );
         const response = await UserModel.findById(object.userid);
         generateWeeklyExpenseEmail(response.email, response.username);
-        console.log(response);
+        logger.info(`Generated Weekly Report for ${object.userid}`);
+      } catch (error) {
+        logger.error(error.message);
+      }
+    });
+  } catch (error) {
+    logger.error(error.message);
+  }
+};
+
+export const generateMonthlyReport = async () => {
+  try {
+    // Gets all expenses
+    const expenses = await ExpensesModel.find({});
+    const data = await expenses;
+    data.map(async (object) => {
+      let totalAmount = 0;
+      let essentialsAmount = 0;
+      let wantsAmount = 0;
+      let cultureAmount = 0;
+      let unexpectedAmount = 0;
+
+      object.expenseList.length > 0 &&
+        object.expenseList
+          .filter(
+            (item) =>
+              item.date > new Date(new Date().getFullYear(), new Date().getMonth()) &&
+              item.date < new Date()
+          )
+          .map((item) => {
+            totalAmount += item.amount;
+            switch (item.category) {
+              case "Essentials":
+                essentialsAmount += item.amount;
+                break;
+              case "Wants":
+                wantsAmount += item.amount;
+                break;
+              case "Culture":
+                cultureAmount += item.amount;
+                break;
+              case "Unexpected":
+                unexpectedAmount += item.amount;
+                break;
+            }
+          });
+
+      try {
+        await ReportsModel.findOneAndUpdate(
+          { userid: object.userid },
+          {
+            $push: {
+              monthlyReports: {
+                totalAmount: totalAmount,
+                essentialsAmount: essentialsAmount,
+                wantsAmount: wantsAmount,
+                cultureAmount: cultureAmount,
+                unexpectedAmount: unexpectedAmount,
+              },
+            },
+          }
+        );
+        const response = await UserModel.findById(object.userid);
+        generateMonthlyExpenseEmail(response.email, response.username);
         logger.info(`Generated Weekly Report for ${object.userid}`);
       } catch (error) {
         logger.error(error.message);
@@ -75,3 +139,26 @@ export const generateWeeklyReport = async () => {
 };
 
 const weekly = new cron.CronJob("00 00 8 * * 0", generateWeeklyReport, null, true, "Europe/London");
+const monthly = new cron.CronJob(
+  `00 00 8 28-31 * *`,
+  () => {
+    if (isLastDayOfMonth()) {
+      generateMonthlyReport();
+    }
+  },
+  null,
+  true,
+  "Europe/London"
+);
+
+function isLastDayOfMonth() {
+  const today = new Date();
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  if (today.getMonth() !== tomorrow.getMonth()) {
+    return true;
+  } else {
+    return false;
+  }
+}
---
+++ server/utils/emailTemplates/monthlyReportTemplate.ts
@@ -0,0 +1,264 @@
+import logger from "../../config/winston";
+import { transporter } from "../email";
+
+export const generateMonthlyExpenseEmail = async (recipientEmail: string, username: string) => {
+  transporter
+    .sendMail({
+      from: `"Personal Finance" ${process.env.EMAIL_USERNAME}`, // sender address
+      to: recipientEmail, // list of receivers
+      subject: "Generated Monthly Report", // Subject line
+      attachments: [
+        {
+          filename: "Logo.png",
+          path: __dirname + "/images/Logo.png",
+          cid: "Logo.png",
+        },
+      ],
+      html: `
+      <head>
+      <title>
+      </title>
+      <!--[if !mso]><!-->
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!--<![endif]-->
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <style type="text/css">
+        #outlook a {
+          padding: 0;
+        }
+    
+        body {
+          margin: 0;
+          padding: 0;
+          -webkit-text-size-adjust: 100%;
+          -ms-text-size-adjust: 100%;
+        }
+    
+        table,
+        td {
+          border-collapse: collapse;
+          mso-table-lspace: 0pt;
+          mso-table-rspace: 0pt;
+        }
+    
+        img {
+          border: 0;
+          height: auto;
+          line-height: 100%;
+          outline: none;
+          text-decoration: none;
+          -ms-interpolation-mode: bicubic;
+        }
+    
+        p {
+          display: block;
+          margin: 13px 0;
+        }
+    
+      </style>
+      <!--[if mso]>
+        <noscript>
+        <xml>
+        <o:OfficeDocumentSettings>
+          <o:AllowPNG/>
+          <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+        </xml>
+        </noscript>
+        <![endif]-->
+      <!--[if lte mso 11]>
+        <style type="text/css">
+          .mj-outlook-group-fix { width:100% !important; }
+        </style>
+        <![endif]-->
+      <!--[if !mso]><!-->
+      <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
+      <style type="text/css">
+        @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+    
+      </style>
+      <!--<![endif]-->
+      <style type="text/css">
+        @media only screen and (min-width:480px) {
+          .mj-column-per-100 {
+            width: 100% !important;
+            max-width: 100%;
+          }
+    
+          .mj-column-per-50 {
+            width: 50% !important;
+            max-width: 50%;
+          }
+        }
+    
+      </style>
+      <style media="screen and (min-width:480px)">
+        .moz-text-html .mj-column-per-100 {
+          width: 100% !important;
+          max-width: 100%;
+        }
+    
+        .moz-text-html .mj-column-per-50 {
+          width: 50% !important;
+          max-width: 50%;
+        }
+    
+      </style>
+      <style type="text/css">
+        @media only screen and (max-width:480px) {
+          table.mj-full-width-mobile {
+            width: 100% !important;
+          }
+    
+          td.mj-full-width-mobile {
+            width: auto !important;
+          }
+        }
+    
+      </style>
+      <style type="text/css">
+      </style>
+    </head>
+    
+    <body style="word-spacing:normal;background-color:rgb(30, 41, 59);">
+      <div style="background-color:rgb(30, 41, 59);">
+        <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" bgcolor="rgb(15, 23, 42)" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+        <div style="background:rgb(15, 23, 42);background-color:rgb(15, 23, 42);margin:0px auto;max-width:600px;">
+          <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:rgb(15, 23, 42);background-color:rgb(15, 23, 42);width:100%;">
+            <tbody>
+              <tr>
+                <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;">
+                  <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
+                  <div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                    <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+                      <tbody>
+                        <tr>
+                          <td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;">
+                              <tbody>
+                                <tr>
+                                  <td style="width:80px;">
+                                    <img alt="Logo" height="80" src="https://drive.google.com/uc?id=17txKhgG6pwhWUhfzjZ_L0BL1uydMgGGn" style="border:0;display:block;outline:none;text-decoration:none;height:80px;width:100%;font-size:13px;" width="80" />
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <!--[if mso | IE]></td></tr></table><![endif]-->
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" bgcolor="rgb(15, 23, 42)" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+        <div style="background:rgb(15, 23, 42);background-color:rgb(15, 23, 42);margin:0px auto;max-width:600px;">
+          <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:rgb(15, 23, 42);background-color:rgb(15, 23, 42);width:100%;">
+            <tbody>
+              <tr>
+                <td style="border-bottom:1px solid rgb(34, 110, 197);border-top:1px solid rgb(34, 110, 197);direction:ltr;font-size:0px;padding:20px 0;text-align:center;">
+                  <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
+                  <div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                    <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+                      <tbody>
+                        <tr>
+                          <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:rgb(255,255,255);">Hello there ${username},</div>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:rgb(255,255,255);">An expense report for the last month has been generated. Login to analyse your spending habits.</div>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td align="center" vertical-align="middle" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:separate;line-height:100%;">
+                              <tbody>
+                                <tr>
+                                  <td align="center" bgcolor="rgb(34, 110, 197)" role="presentation" style="border:none;border-radius:3px;cursor:auto;mso-padding-alt:10px 25px;background:rgb(34, 110, 197);" valign="middle">
+                                    <a href="https://marglipersonalfinance.me" style="display:inline-block;background:rgb(34, 110, 197);color:#ffffff;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;font-weight:normal;line-height:120%;margin:0;text-decoration:none;text-transform:none;padding:10px 25px;mso-padding-alt:0px;border-radius:3px;" target="_blank"> Go to website </a>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:rgb(255,255,255);">Thank you,</div>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:rgb(255,255,255);">Marius G.</div>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <!--[if mso | IE]></td></tr></table><![endif]-->
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" bgcolor="rgb(15, 23, 42)" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+        <div style="background:rgb(15, 23, 42);background-color:rgb(15, 23, 42);margin:0px auto;max-width:600px;">
+          <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:rgb(15, 23, 42);background-color:rgb(15, 23, 42);width:100%;">
+            <tbody>
+              <tr>
+                <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;">
+                  <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:300px;" ><![endif]-->
+                  <div class="mj-column-per-50 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                    <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+                      <tbody>
+                        <tr>
+                          <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:rgb(255,255,255);">Vilnius, Lithuania</div>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:#000000;"><a href="marglipersonalfinance.me" style="color: rgb(255,255,255)">marglipersonalfinance.me</a></div>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <!--[if mso | IE]></td><td class="" style="vertical-align:top;width:300px;" ><![endif]-->
+                  <div class="mj-column-per-50 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                    <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+                      <tbody>
+                        <tr>
+                          <td align="right" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:right;color:rgb(255,255,255);"><a href="https://tosdr.org/" style="color: rgb(255,255,255)">Privacy Statement</a></div>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <!--[if mso | IE]></td></tr></table><![endif]-->
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <!--[if mso | IE]></td></tr></table><![endif]-->
+      </div>
+    </body>
+      `,
+    })
+    .then((info) => {
+      logger.info({
+        recipient: info.accepted[0],
+        response: info.response,
+        messageId: info.messageId,
+      });
+    })
+    .catch((error) => logger.error(error.message));
+};
