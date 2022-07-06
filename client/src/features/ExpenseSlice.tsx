17c5d50926cc05f773d4875c6d368fb76d7bbe4e
Marius <margliaudelis@gmail.com>
2022-07-06 23:10:56 +0300

    Add expense sorting

--- client/src/features/ExpenseSlice.tsx
+++ client/src/features/ExpenseSlice.tsx
@@ -1,6 +1,7 @@
 import { ExpenseFetchState, ExpenseState } from "../models/expenses";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
+import { DateTime } from "luxon";
 import { expensesApi } from "../services/expenses";
 
 // Redux Toolkit created expense slice
@@ -10,7 +11,55 @@ export const expenseSlice = createSlice({
   name: "expenses",
   initialState: { data: [] } as ExpenseFetchState,
   reducers: {
-    sortByDate: (state: ExpenseFetchState) => {},
+    sortByDate: (
+      state: ExpenseFetchState,
+      action?: PayloadAction<{ sorted: boolean }>
+    ) => {
+      if (!action?.payload.sorted) {
+        const newArray = state.data.sort((a, b) => {
+          if (DateTime.fromISO(a.date) > DateTime.fromISO(b.date)) {
+            return 1;
+          } else if (DateTime.fromISO(a.date) < DateTime.fromISO(b.date)) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+        state.data = newArray;
+      } else {
+        state.data = state.data.reverse();
+      }
+    },
+    sortByCategory: (
+      state: ExpenseFetchState,
+      action?: PayloadAction<{ sorted: boolean }>
+    ) => {
+      if (!action?.payload.sorted) {
+        const newArray = state.data.sort((a, b) => {
+          if (a.category > b.category) return -1;
+          else if (a.category < b.category) return 1;
+          else return 0;
+        });
+        state.data = newArray;
+      } else {
+        state.data = state.data.reverse();
+      }
+    },
+    sortByAmount: (
+      state: ExpenseFetchState,
+      action?: PayloadAction<{ sorted: boolean }>
+    ) => {
+      if (!action?.payload.sorted) {
+        const newArray = state.data.sort((a, b) => {
+          if (a.amount > b.amount) return -1;
+          else if (a.amount < b.amount) return 1;
+          else return 0;
+        });
+        state.data = newArray;
+      } else {
+        state.data = state.data.reverse();
+      }
+    },
   },
   extraReducers: (builder) => {
     builder.addMatcher(
@@ -26,6 +75,7 @@ export const expenseSlice = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-//export const {} = expenseSlice.actions;
+export const { sortByDate, sortByCategory, sortByAmount } =
+  expenseSlice.actions;
 
 export default expenseSlice.reducer;
--- client/src/routes/Dashboard/Dashboard.tsx
+++ client/src/routes/Dashboard/Dashboard.tsx
@@ -1,9 +1,44 @@
 import "./Dashboard.scss";
 
+import {
+  sortByAmount,
+  sortByCategory,
+  sortByDate,
+} from "../../features/ExpenseSlice";
+
 import { ChartContainer } from "../../layout/ChartContainer/ChartContainer";
 import { ExpenseList } from "../../layout/ExpenseList/ExpenseList";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 export const Dashboard = () => {
+  const [sortedByDate, setSortedByDate] = useState<boolean>(false);
+  const [sortedByCategory, setSortedByCategory] = useState<boolean>(false);
+  const [sortedByAmount, setSortedByAmount] = useState<boolean>(false);
+
+  const dispatch = useDispatch();
+
+  const categorySort = () => {
+    dispatch(sortByCategory({ sorted: sortedByCategory }));
+    setSortedByCategory(!sortedByCategory);
+    setSortedByDate(false);
+    setSortedByAmount(false);
+  };
+
+  const dateSort = () => {
+    dispatch(sortByDate({ sorted: sortedByDate }));
+    setSortedByDate(!sortedByDate);
+    setSortedByCategory(false);
+    setSortedByAmount(false);
+  };
+
+  const amountSort = () => {
+    dispatch(sortByAmount({ sorted: sortedByAmount }));
+    setSortedByAmount(!sortedByAmount);
+    setSortedByDate(false);
+    setSortedByCategory(false);
+  };
+
   // Redux Toolkit api Request to get expense array
   return (
     <>
@@ -12,9 +47,15 @@ export const Dashboard = () => {
         <ChartContainer />
         <div className="expenseList">
           <div className="listHeader">
-            <div className="expenseListHeaderDate">Date</div>
-            <div className="expenseListHeaderCategory">Category</div>
-            <div className="expenseListHeaderAmount">Amount</div>
+            <div className="expenseListHeaderDate" onClick={dateSort}>
+              Date
+            </div>
+            <div className="expenseListHeaderCategory" onClick={categorySort}>
+              Category
+            </div>
+            <div className="expenseListHeaderAmount" onClick={amountSort}>
+              Amount
+            </div>
             <div className="expenseListHeaderDescription">Description</div>
             <div className="expenseListHeaderTags">Tags</div>
             <div className="expenseListHeaderControls">Edit</div>
