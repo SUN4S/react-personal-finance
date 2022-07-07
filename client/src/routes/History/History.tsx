aa17a55aea5c4f889f3efadeb1b6c719e95c1601
Marius <margliaudelis@gmail.com>
2022-07-07 15:27:32 +0300

    Add sorting functionality in history

--- client/src/routes/History/History.tsx
+++ client/src/routes/History/History.tsx
@@ -1,16 +1,64 @@
 import "./History.scss";
 
+import {
+  sortByAmount,
+  sortByCategory,
+  sortByDate,
+} from "../../features/HistorySlice";
+
 import { HistoryList } from "../../layout/HistoryList/HistoryList";
 import { ImageModal } from "../../layout/ImageModal/ImageModal";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 export const History = () => {
+  // States to monitor what type of sorting was used
+  const [sortedByDate, setSortedByDate] = useState<boolean>(false);
+  const [sortedByCategory, setSortedByCategory] = useState<boolean>(false);
+  const [sortedByAmount, setSortedByAmount] = useState<boolean>(false);
+
+  // Redux toolkit function to call reducer functions
+  const dispatch = useDispatch();
+
+  // Sorting Expense array list by Date
+  const dateSort = () => {
+    // Dispatching reducer function
+    dispatch(sortByDate({ sorted: sortedByDate }));
+    setSortedByDate(!sortedByDate);
+    setSortedByCategory(false);
+    setSortedByAmount(false);
+  };
+
+  // Sorting Expense array list by Category
+  const categorySort = () => {
+    // Dispatching reducer function
+    dispatch(sortByCategory({ sorted: sortedByCategory }));
+    setSortedByCategory(!sortedByCategory);
+    setSortedByDate(false);
+    setSortedByAmount(false);
+  };
+
+  // Sorting Expense array list by Amount
+  const amountSort = () => {
+    // Dispatching reducer function
+    dispatch(sortByAmount({ sorted: sortedByAmount }));
+    setSortedByAmount(!sortedByAmount);
+    setSortedByDate(false);
+    setSortedByCategory(false);
+  };
   return (
     <section className="history">
       <div className="historyContent">
         <div className="historylistHeader">
-          <div className="historyListHeaderDate">Date</div>
-          <div className="historyListHeaderCategory">Category</div>
-          <div className="historyListHeaderAmount">Amount</div>
+          <div className="historyListHeaderDate" onClick={dateSort}>
+            Date
+          </div>
+          <div className="historyListHeaderCategory" onClick={categorySort}>
+            Category
+          </div>
+          <div className="historyListHeaderAmount" onClick={amountSort}>
+            Amount
+          </div>
           <div className="historyListHeaderDescription">Description</div>
           <div className="historyListHeaderTags">Tags</div>
           <div className="historyListHeaderControls">Receipt</div>
